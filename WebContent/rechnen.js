fe9f6409b3679acd44efe58a69ee10443a3d8736
Türkyilmaz Sergen <sergen.tuerkyilmaz@live.de>
2017-10-25 15:57:38 +0200

    Taschenrechner abgeschlossen, jquery getestet

--- WebContent/rechnen.js
+++ WebContent/rechnen.js
@@ -1,26 +1,88 @@
+//erste Zeile
 
-function run1(){
+function add1(){
 	
-	document.frm.result.value += "1";
+	document.calculator.answer.value += '1';
 }
 
-function run2(){
+function add2(){
 	
-	document.frm.result.value += "2";
+	document.calculator.answer.value += '2';
 }
 
-function run3(){
+function add3(){
 	
-	document.frm.result.value += "3";
+	document.calculator.answer.value += '3';
 }
 function runplus(){
 	
-	document.frm.result.value += "+";
+	document.calculator.answer.value += '+'
 }
 
-function evalu(){
+//zweite Zeile
+
+function add4(){
 	
-	var evalo = eval(document.frm.result.value)
-	
-	document.frm.result.value = evalo;
+	document.calculator.answer.value += '4';
 }
+
+function add5(){
+	
+	document.calculator.answer.value += '5';
+}
+
+function add6(){
+	
+	document.calculator.answer.value += '6';
+}
+function runminus(){
+	
+	document.calculator.answer.value += '-';
+}
+
+//dritte Zeile
+
+function add7(){
+	
+	document.calculator.answer.value += '7';
+}
+
+function add8(){
+	
+	document.calculator.answer.value += '8';
+}
+
+function add9(){
+	
+	document.calculator.answer.value += '9';
+}
+function runmulti(){
+	
+	document.calculator.answer.value += '*';
+}
+
+//vierte Zeile
+
+function runclear(){
+	
+
+	document.calculator.answer.value = '';
+}
+
+function add0(){
+	
+	document.calculator.answer.value += '0';
+}
+
+function runsolution(){
+	
+	document.calculator.answer.value = eval(calculator.answer.value);
+}
+function rundivide(){
+	
+	document.calculator.answer.value += '/';
+}
+
+
+
+
